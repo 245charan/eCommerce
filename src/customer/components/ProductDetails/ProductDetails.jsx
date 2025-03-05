488b3e58ff3daceab8661e2f5ff9ce07875d2791
Charan-Epuri <Charan.Epuri1@marist.edu>
2025-03-04 21:15:40 -0500

    implemented order page, order detail page, and added routes

--- src/customer/components/ProductDetails/ProductDetails.jsx
+++ src/customer/components/ProductDetails/ProductDetails.jsx
@@ -8,6 +8,7 @@ import { Box, Button, Grid, LinearProgress } from "@mui/material";
 import ProductReviewCard from "./ProductReviewCard.jsx";
 import { mens_kurta } from "../../../Data/mens_kurta.js";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard.jsx";
+import { useNavigate } from "react-router-dom";
 const product = {
 	name: "Basic Tee 6-Pack",
 	price: "$192",
@@ -65,6 +66,11 @@ function classNames(...classes) {
 export default function ProductDetails() {
 	const [selectedColor, setSelectedColor] = useState(product.colors[0]);
 	const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+	const navigate = useNavigate();
+
+	const handleAddtoCart=()=>{
+		navigate('/cart')
+	}
 
 	return (
 		<div className='bg-white lg:px-20'>
@@ -206,8 +212,8 @@ export default function ProductDetails() {
 									</fieldset>
 								</div>
 
-								<Button variant='contained' sx={{ px: "2rem", py: "1rem", bgcolor: "#9155fd" }}>
-									Add to bag
+								<Button onClick={handleAddtoCart} variant='contained' sx={{ px: "2rem", py: "1rem", bgcolor: "#9155fd" }}>
+									Add to Cart
 								</Button>
 							</form>
 						</div>
